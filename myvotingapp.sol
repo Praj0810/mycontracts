107d21c6c75bae6d5c0b5fd39778540507a75355
= <=>
2022-07-11 17:35:44 +0530

    "mysmart contract assignment"

--- myvotingapp.sol
+++ myvotingapp.sol
@@ -1,66 +1,106 @@
-pragma solidity ^0.4.0;
-contract MyVotingApp{
-    //Structure for candidate participating in Election:
-    struct Candidate{
-        string name;
-        uint voteCount;
-    }
-    //Structure for Voter voting the candidate :
-    struct Voter{
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+/****
+ *@title A Smart Contract of VoteApp
+ *@author Prajakta Mohite
+ *@dev Authorized the address votting for Trump or Biden
+ *@notice This contract gives the result of the vottingpoll between Trump and Biden
+ */
+contract MyVotingApp {
+
+    struct Voter {
         bool authorized;
-        bool voted;
-        uint voteIndex;
-        uint weight;
     }
-    
-    
+
     address public contractowner;
-    string public ElectionName;
     mapping(address => Voter) public voters;
-    Candidate[] public candidates;
-    uint public totalvotes;
-    
-    //access authorized by contractowner only;
-    modifier Access(){
-        require(msg.sender == contractowner);
+    mapping(address => bool) alreadyVoted;
+
+
+    uint256 votesForTrump;
+    uint256 votesForBiden;
+    uint256 startVote;
+    uint256 endVote;
+
+    modifier Access(address _person) {
+        require(msg.sender == contractowner, "ownership of contract");
+        require(!voters[_person].authorized, "The voters is already Authorized");
         _;
     }
 
-    constructor(string _name) public{
+    modifier VoteTime() {
+        require(block.timestamp >= startVote, "Voting not started");
+        require(block.timestamp <= endVote, "Voting has ended");
+        _;
+    }
+
+    constructor(uint256 _startVote, uint256 _endVote) {
+        require(
+            _startVote < _endVote,
+            "Start time of vote should be lesser than Endtime"
+        );
         contractowner = msg.sender;
-        ElectionName = _name;  
+        startVote = block.timestamp + _startVote;
+        endVote = startVote + _endVote;
+    }
+
+    /**
+     *@notice This function will authorised the voters
+     *@dev Address voters voting for Trump or biden are authorized by the contract owner
+     *@param _person This parameter use as a address of different voters
+     */
+    function authorize(address _person) public Access(_person) {
+        voters[_person].authorized = true;
     }
 
 
-    function addCandidate(string candidate1, string candidate2) Access public{
-        candidates.push(Candidate(candidate1, 0));
-        candidates.push(Candidate(candidate2,0));
-    }
-    
-    function authorize(address voter) Access public {
-        require(!voters[voter].voted);
-        require(voters[msg.senderz].authorized);
-        voters[msg.sender].authorized = true;
-        voters[voter].weight = 1;   
-    }
-
-    function vote(uint _voteIndex) public{
-
-        require(!voters[msg.sender].voted);
-        require(voters[msg.sender].authorized);
-
-        voters[msg.sender].voted = true;
-        voters[msg.sender].voteIndex = _voteIndex;
-
-        candidates[_voteIndex].voteCount += voters[msg.sender].weight;
-
-        totalvotes += 1;
-
-    }
-
-    function endElection() public {
-        selfdestruct(contractowner);
+    /**
+     *@notice Person Authorized will vote for Trump
+     *@dev In this function  voter will vote in the given voting time and check if the voters has already voted
+     */
+    function VotingforTrump() public VoteTime {
+        require(!alreadyVoted[msg.sender], "already voted");
+        votesForTrump += 1;
+        alreadyVoted[msg.sender] = true;
     }
 
 
+
+    /**
+     *@notice Person Authorized will vote for Biden
+     *@dev Person Authorized will vote for Biden
+     */
+    function VotingforBiden() public VoteTime {
+        require(!alreadyVoted[msg.sender], "already voted");
+        votesForBiden += 1;
+        alreadyVoted[msg.sender] = true;
+    }
+
+
+    /**
+    *@notice This function gives the votes count of the overall votepoll 
+    *@dev The Totalvotes of overall Votepoll is being calculated by adding Votes of trump and biden
+    *@return totalvotes uint
+    */
+    function votesCounts() public view returns (uint256) {
+        uint256 totalvotes = (votesForTrump + votesForBiden);
+        return totalvotes;
+    }
+
+
+    /**
+     *@notice This function gives the winner of the votingpoll 
+     *@dev This function gives the final result of the winner and the count of votes the Candidate won by
+     */
+    function VotingWinner() public view returns (string memory, uint256) {
+        require(block.timestamp >= endVote, " Voting results not ready");
+        if (votesForBiden > votesForTrump) {
+            return ("Winner is Biden ", (votesForBiden - votesForTrump));
+        } else if (votesForTrump > votesForBiden) {
+            return ("Winner is Trump", (votesForTrump - votesForBiden));
+        } else {
+            return ("Tie", (votesForTrump - votesForBiden));
+        }
+    }
 }
